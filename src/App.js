d145e9dd6d5ac9f5197bf63c47b01cfed607d608
anjali <anjalichanda2019@gmail.com>
2025-01-15 11:39:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import AboutUs from './components/AboutUs';
+import Faq from './components/Faq';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import LatestNews from './components/LatestNews';
+import OurStory from './components/OurStory';
+import Roadmap from './components/Roadmap';
+import Team from './components/Team';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <OurStory />
+      <AboutUs />
+      <Roadmap />
+      <Team />
+      <Faq />
+      <LatestNews />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/utils/helper.js
@@ -0,0 +1,5 @@
+import { DiscordIcons, InstagramIcons, TwitterIcons } from "./icons";
+
+export const SOCIAL_ICONS_LIST = [
+    <TwitterIcons />, <InstagramIcons />, <DiscordIcons />
+]
